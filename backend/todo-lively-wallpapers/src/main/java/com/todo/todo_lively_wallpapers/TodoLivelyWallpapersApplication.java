ea3c6d075fe81604814302f21963ae112e9e3ad0
SushrutaBera <sushurutabera1502@gmail.com>
2025-02-18 22:10:13 +0530

    Initial Commit

---
+++ backend/todo-lively-wallpapers/src/main/java/com/todo/todo_lively_wallpapers/TodoLivelyWallpapersApplication.java
@@ -0,0 +1,13 @@
+package com.todo.todo_lively_wallpapers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoLivelyWallpapersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoLivelyWallpapersApplication.class, args);
+	}
+
+}
---
+++ backend/todo-lively-wallpapers/src/test/java/com/todo/todo_lively_wallpapers/TodoLivelyWallpapersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.todo.todo_lively_wallpapers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoLivelyWallpapersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
