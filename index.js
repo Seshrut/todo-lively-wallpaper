95217cfce37bc587637a59c6f3579544ed7d7e44
Seshrut <seshrut@gmail.com>
2025-05-18 15:32:14 +0530

    ui for settings

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
 var images
 var last5=[]
+
 window.onload = async ()=>{
+    var setting = document.getElementById("setting");
+    var closeico = document.getElementById("close");
+    var gearico = document.getElementById("gear");
+    var settingmenu = document.getElementById("settingmenu");
+    var menuvis=true;
+    setting.addEventListener('click',()=>{
+        if(menuvis){
+            closeico.style.display='block';
+            gearico.style.display='none';
+            settingmenu.style.display='flex';
+            menuvis=false;
+        }
+        else{
+            closeico.style.display='none';
+            gearico.style.display='block';
+            settingmenu.style.display='none';
+            menuvis=true;
+        }
+    });
     // try to load images in a folder
     images = await window.onstart.getImg();
     const body=document.querySelector('body');
