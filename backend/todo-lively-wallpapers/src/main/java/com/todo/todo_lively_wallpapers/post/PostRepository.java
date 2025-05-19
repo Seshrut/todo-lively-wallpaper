f85f780aec198340acc6882328b6abe05d11f60d
SushrutaBera <sushrutabera1502@gmail.com>
2025-05-19 11:22:59 +0530

    Added OAuth 2.0...still working on it tho...

--- backend/todo-lively-wallpapers/src/main/java/com/todo/todo_lively_wallpapers/post/PostRepository.java
+++
@@ -1,9 +0,0 @@
-package com.todo.todo_lively_wallpapers.post;
-
-import org.springframework.data.repository.ListCrudRepository;
-
-import java.util.List;
-
-public interface PostRepository extends ListCrudRepository<Post, String>{
-
-}
